9942a19c3cf0f4e78e529f1687dfa31a8b232754
Tainá Meirelles <tainaramosmeirelles@gmail.com>
2025-05-26 20:38:40 -0300

    Add files via upload

--- loop/script.js
+++ loop/script.js
@@ -1,7 +1,14 @@
 function calcularIdade(){
-
+    
 }
 function calcularOperador(){
+    let numero1 = Number(document.getElementById("number1").value)
+    let numero2 = Number(document.getElementById("number2").value)
+    let operador = (document.getElementById(operador).value)
+    let resultadocalc
 
-    
+    switch(operador){
+        
+    }
+
 }
